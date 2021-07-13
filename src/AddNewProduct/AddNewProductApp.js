52f214d95449aed39d4eeff5ebe279e248ef3d47
yunjs24 <85775822+yunjs24@users.noreply.github.com>
2021-07-13 12:14:24 +0900

    Feature/getorderlist (#11)
    * 동기화

--- src/AddNewProduct/AddNewProductApp.js
+++ src/AddNewProduct/AddNewProductApp.js
@@ -3,15 +3,6 @@ import axios from "axios";
 import "./Option.css";
 // import Option from "./Option";
 //import DisplayProductName from "./DisplayProductName";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import CategoryForm from "./CategoryForm";
-=======
-//import CategoryForm from "./CategoryForm";
->>>>>>> b79b38ffb17383133f8583f841d34bf55b85d799
-=======
-//import CategoryForm from "./CategoryForm";
->>>>>>> 660d990aab8a11989cbf37aacbcc31ccdae8d636
 // import ImgUpload from "./ImgUpload";
 // import WriteProductDetail from "./WriteProductDetail";
 // import NewProductConfirmButton from "./NewProductConfirmButton";
